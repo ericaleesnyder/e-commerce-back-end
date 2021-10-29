11badf76bedf4d102e7e7c3659c78af1b7120db6
Erica Snyder <ericaleesnyder@gmail.com>
2021-10-29 16:44:39 -0700

    added query to get one tag by id, created post query to create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -18,13 +18,30 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [
+        { model: Product, through: ProductTag, as: "products" }
+      ]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const dbTagData = await Tag.create(req.body);
+    res.status(200).json(dbTagData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
