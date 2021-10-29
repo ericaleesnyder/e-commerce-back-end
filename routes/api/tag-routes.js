288fe4b8919366a5a9f7c0b7ad4c52a996bad7e7
Erica Snyder <ericaleesnyder@gmail.com>
2021-10-29 16:50:33 -0700

    added delete tag query

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -48,8 +48,20 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: { id: req.params.id }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: "No tag with this id!" });
+      return;
+    }
+    res.status(200).json({ message: "Tag deleted successfully." });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
